f3b267a97e2584206576cea7341c687e4ae8cd45
najmus2808 <najmus2808@gmail.com>
2021-02-06 19:44:24 -0800

    inchTofeet Conversion

---
+++ Js_problem_solve/inchTofeet.js
@@ -0,0 +1,16 @@
+// *** Without function ***
+var inch = 134;
+var feet = Math.round(inch/12);
+inch = inch % 12;
+console.log(feet + " feet " + inch + " inch");
+
+// ### With function ###
+
+function inchTofeet(inch){
+    var feet = Math.round(inch/12);
+    inch = inch % 12;
+    console.log(feet + " feet " + inch + " inch");
+}
+
+inchTofeet(144);
+inchTofeet(177);
