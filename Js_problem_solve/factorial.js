1537f91926b47c3610deda94e2e07f51b3e6ac99
najmus2808 <najmus2808@gmail.com>
2021-02-18 19:33:33 -0800

    added 0!

--- Js_problem_solve/factorial.js
+++ Js_problem_solve/factorial.js
@@ -1,18 +1,27 @@
+// 0! = 1
 // 1! = 1
 // 2! = 1 * 2
 // 3! = 1 * 2 * 3
 // 4! = 1 * 2 * 3 * 4
 // 5! = 1 * 2 * 3 * 4 * 5
 
-function factorial(n){
-    var factorial = 1;
-    for(var i = 1; i<=n; i++){
-         factorial = factorial * i ;
+function factorial(n) {
+    if (n == 0) {
+        return 1;
     }
+    else {
+        var factorial = 1;
+        for (var i = 1; i <= n; i++) {
+            factorial = factorial * i;
+        }
 
-    console.log(factorial);
+        console.log(factorial);
+    }
 }
 
+var result = factorial(0);
+
+console.log("0! = " + " " + result);
 factorial(1);
 factorial(2);
 factorial(3);
