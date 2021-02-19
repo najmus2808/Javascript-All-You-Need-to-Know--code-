f5ca9e22837376b167d1d5aff6888c9b370c66cb
najmus2808 <najmus2808@gmail.com>
2021-02-18 19:10:17 -0800

    factorial of n

---
+++ Js_problem_solve/factorial.js
@@ -0,0 +1,20 @@
+// 1! = 1
+// 2! = 1 * 2
+// 3! = 1 * 2 * 3
+// 4! = 1 * 2 * 3 * 4
+// 5! = 1 * 2 * 3 * 4 * 5
+
+function factorial(n){
+    var factorial = 1;
+    for(var i = 1; i<=n; i++){
+         factorial = factorial * i ;
+    }
+
+    console.log(factorial);
+}
+
+factorial(1);
+factorial(2);
+factorial(3);
+factorial(4);
+factorial(5);
