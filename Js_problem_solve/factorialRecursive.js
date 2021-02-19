3a7ab1e92f8998451cd32020943f3f5f740a948c
najmus2808 <najmus2808@gmail.com>
2021-02-18 19:57:30 -0800

    factorial with Recursive

---
+++ Js_problem_solve/factorialRecursive.js
@@ -0,0 +1,23 @@
+// 0! = 1
+// 1! = 1
+// 2! = 1 * 2
+// 3! = 1 * 2 * 3
+// 4! = 1 * 2 * 3 * 4
+// 5! = 1 * 2 * 3 * 4 * 5
+// 6! = 5! * 6
+// 6! = (6-1)! * 6
+// n! = (n-1)! * n
+// n! = n * (n-1)
+
+
+function factorialRecursive(n){
+    if(n == 0){
+        return 1;
+    }
+    else{
+        return n * factorialRecursive(n - 1);
+    }
+}
+
+var result = factorialRecursive(10);
+console.log(result);
