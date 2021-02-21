859480b3db8428dc8170155f9b44317d4ea4459a
najmus2808 <najmus2808@gmail.com>
2021-02-20 20:51:48 -0800

    arrow function behaviour with this keyword

---
+++ Modern_Javascript_Syntax_ES6/arrowFunctionWithThis.js
@@ -0,0 +1,47 @@
+/** In this code "this" keyword don't get it's proper value 
+because forEach build in function change the value of "this" 
+that's why we cann't get expected output **/
+// let javascript = {
+//     name: "Javascript",
+//     libraries: ["React", "Angular", "Vue"],
+//     printLibraries: function () {
+//         this.libraries.forEach(function(p){
+//             console.log(`${this.name} loves ${p}`);
+//         })
+//     }
+// }
+
+// javascript.printLibraries();
+
+
+/** Solve this problem - one way */
+
+// let javascript = {
+//     name: "Javascript",
+//     libraries: ["React", "Angular", "Vue"],
+//     printLibraries: function () {
+//         let self = this; // added one line
+//         this.libraries.forEach(function(p){
+//             console.log(`${self.name} loves ${p}`); // replace this = self
+//         })
+//     }
+// }
+
+// javascript.printLibraries();
+
+
+/**Best way to solve this problem using arrow function... Arrow function get the proper value of "this" in this environment **/
+
+let javascript = {
+    name: "Javascript",
+    libraries: ["React", "Angular", "Vue"],
+    printLibraries: function () {
+        this.libraries.forEach((p) => {      // Using arrow function in this line
+            console.log(`${this.name} loves ${p}`);
+        })
+    }
+}
+
+javascript.printLibraries();
+
+
