ab91035c3f3d3cce9c59c7db6b142a5c0bd76652
najmus2808 <najmus2808@gmail.com>
2021-03-01 09:22:18 -0800

    sum of the array

---
+++ Js_problem_solve/arraySum.js
@@ -0,0 +1,9 @@
+//Find Sum of the array 
+
+var numbers = [23, 43, 56, 12, 87, 50];
+var sum = 0;
+for(var i = 0; i < numbers.length; i++){
+    sum += numbers[i];
+}
+
+console.log("Total: " + sum);
