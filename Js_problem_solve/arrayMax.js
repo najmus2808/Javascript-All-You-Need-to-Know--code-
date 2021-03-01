488999a46ceaae15dfcfe5c039b9f6a08ee38d15
najmus2808 <najmus2808@gmail.com>
2021-03-01 09:09:52 -0800

    find the largest element of an array

---
+++ Js_problem_solve/arrayMax.js
@@ -0,0 +1,10 @@
+// Find the largest element of an array
+
+var marks = [15, 34, 54, 5, 3, 87, 1];
+var max = marks[0];
+for(var i = 1; i<marks.length; i++){
+    if(max < marks[i]){
+        max = marks[i];
+    } 
+}
+console.log("Largest element is: " + max);
