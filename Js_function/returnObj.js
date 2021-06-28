1c7417d383e90e983c22fca56968bd6241c3ea5a
najmus2808 <najmus2808@gmail.com>
2021-06-28 12:54:29 +0600

    return a object in a funtion

---
+++ Js_function/returnObj.js
@@ -0,0 +1,11 @@
+function returnObj(name, email){
+    const obj = {
+        name:name,
+        email:email
+    }
+
+    return obj
+}
+
+const result = returnObj("Sakib", "najmus2808@gmail.com")
+console.log(result)
