9d590e49207fe7d320ab95f7fa6065750ae6587f
najmus2808 <najmus2808@gmail.com>
2021-06-29 12:04:28 +0600

    example of higher p

--- Js_function/firstClassFunction.js
+++ Js_function/firstClassFunction.js
@@ -15,21 +15,21 @@ console.log(typeof sum)
 var array = []
 array.push(add)
 console.log(array)
-console.log(array[0](5,4))
+console.log(array[0](5, 4))
 // 3. A Function can be Stored in an object
 
-var object ={
+var object = {
     sum: add
 }
 
-console.log(object.sum(3,2))
+console.log(object.sum(3, 2))
 
 // 4. we can create Function as we need
 
-setTimeout(function() {
+setTimeout(function () {
     console.log('I have created a new function')
-},2000)
+}, 2000)
 
-// 5. we can pass function as an arguments (next example)
+// 5. we can pass function as an arguments (shown in example of higher order functions)
 
-// 6. we can return functions from another function (next example)
+// 6. we can return functions from another function  (shown in example of higher order functions)
---
+++ Js_function/higherOrderFunction.js
@@ -0,0 +1,19 @@
+function add(num1, num2) {
+    return num1 + num2
+}
+
+function manipulate(a, b, func) {
+    var c = a + b
+    var d = a - b
+
+    function multiply() {
+        var m = func(a, b)
+        return c * d * m
+    }
+
+    return multiply
+}
+
+var multiply = manipulate(10, 5, add)
+console.log(multiply)
+console.log(multiply())
