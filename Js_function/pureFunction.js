d34946b286bc495115db27ab2ddd8c4a34c94b11
najmus2808 <najmus2808@gmail.com>
2021-06-29 11:28:19 +0600

    example of pure function

---
+++ Js_function/pureFunction.js
@@ -0,0 +1,10 @@
+// 1. It Returns the same result if give the same arguments
+//2. It does not cause any side effects in this whole program(ei function er karone bairer kono kichur man change hobe na)
+
+function sqrt(number) {
+    return number * number
+}
+
+console.log(sqrt(6))
+console.log(sqrt(6))
+console.log(sqrt(6))
