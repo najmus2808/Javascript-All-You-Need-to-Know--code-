4d0663dd95b69173c262cac96d4dbb28cbbe922a
najmus2808 <najmus2808@gmail.com>
2021-06-30 20:42:19 +0600

    Implimentation of reduce method using callback

---
+++ Js_function/reduce.js
@@ -0,0 +1,42 @@
+var array = [1, 2, 70, 3, 4, 5];
+
+// var result = array.reduce(function (acc, current) {
+//     return acc + current
+// },100)
+
+// console.log(result)
+
+// === implementation of reduce method using callback ===
+
+function reduce(arr, cb, acc) {
+  for (var i = 0; i < arr.length; i++) {
+    acc = cb(acc, arr[i]);
+  }
+  return acc;
+}
+
+var sum = reduce(
+  array,
+  function (acc, current) {
+    return acc + current;
+  },
+  0
+);
+
+var max = reduce(
+  array,
+  function (acc, current) {
+    return Math.max(acc, current);
+  },
+  0
+);
+
+var min = reduce(
+  array,
+  function (acc, current) {
+    return Math.min(acc, current);
+  },
+  15
+);
+
+console.log(sum, max, min);
