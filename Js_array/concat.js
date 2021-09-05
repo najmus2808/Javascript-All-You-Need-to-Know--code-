248308551308b5fc28e233c45196a14383493232
najmus2808 <najmus2808@gmail.com>
2021-09-05 17:29:54 +0600

    use of array concat method

---
+++ Js_array/concat.js
@@ -0,0 +1,7 @@
+const myGirls = ["Cecilie", "Lone"];
+const myBoys = ["Emil", "Tobias", "Linus"];
+
+// Concatenate (join) myGirls and myBoys
+const myChildren = myGirls.concat(myBoys);  
+
+console.log(myChildren);
