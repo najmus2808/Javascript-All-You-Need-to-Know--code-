3269443525f67fd1bd704bfe1c2e233154089d74
najmus2808 <najmus2808@gmail.com>
2021-09-07 10:45:01 +0600

    added comments

--- Js_array_iterate_methods/map.js
+++ Js_array_iterate_methods/map.js
@@ -7,15 +7,17 @@ The map() method does not change the original array.
 ***/
 
 const numbers = [45, 4, 9, 16, 25];
-
+]
+//Procedure 1
 const result = numbers.map((element, index, array) => element * 2);
 
-console.log("result =>",result);
+console.log("result =>", result);
 
+//Procedure 2
 const result2 = numbers.map(test);
 
 function test(element, index, array) {
   return element * 4;
 }
 
-console.log("result2 =>",result2);
+console.log("result2 =>", result2);
