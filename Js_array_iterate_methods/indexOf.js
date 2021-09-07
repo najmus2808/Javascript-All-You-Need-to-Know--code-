8e4393cbed8f9cd5f3a9eed1745f5e755e3b3544
najmus2808 <najmus2808@gmail.com>
2021-09-07 12:34:54 +0600

    add some comments

--- Js_array_iterate_methods/indexOf.js
+++ Js_array_iterate_methods/indexOf.js
@@ -1,4 +1,11 @@
-// The indexOf() method searches an array for an element value and returns its position that means index number
+/**The indexOf() method searches an array for an element value and returns its position that means index number
+item	Required. The item to search for.
+start	Optional. Where to start the search. Negative values will start at the given position counting from the end, and search to the end.
+Array.indexOf() returns -1 if the item is not found.
+
+If the item is present more than once, it returns the position of the first occurrence.
+**/
+
 const fruits = ["Apple", "Orange", "Apple", "Mango"];
 let position = fruits.indexOf("Apple") + 1;
 
