15f82437a9d83d04249274c9d19831d4d365928f
najmus2808 <najmus2808@gmail.com>
2021-09-07 14:40:03 +0600

    example of form which create a string from  Array

---
+++ Js_array_iterate_methods/form.js
@@ -0,0 +1,9 @@
+/**
+ Array.from()
+The Array.from() method returns an Array object from any object with a length property or any iterable object.
+Create an Array from a String:
+ */
+
+const myArr = Array.from("ABCDEFG");
+
+console.log(myArr);
