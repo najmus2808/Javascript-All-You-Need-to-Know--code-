8988d0198d5f307fe9263ac037ef552d519a0df5
najmus2808 <najmus2808@gmail.com>
2021-09-07 12:22:15 +0600

    example of indexOf method

---
+++ Js_array_iterate_methods/indexOf.js
@@ -0,0 +1,5 @@
+// The indexOf() method searches an array for an element value and returns its position that means index number
+const fruits = ["Apple", "Orange", "Apple", "Mango"];
+let position = fruits.indexOf("Apple") + 1;
+
+console.log(`Position = ( index + 1) = ${position}`);
