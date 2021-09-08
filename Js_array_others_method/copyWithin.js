2a2b2bbc229261ea11c9b8864b0d0200a522a937
najmus2808 <najmus2808@gmail.com>
2021-09-08 11:23:16 +0600

    add some extra comments

--- Js_array_others_method/copyWithin.js
+++ Js_array_others_method/copyWithin.js
@@ -1,11 +1,14 @@
 /**
 The copyWithin() method copies array elements to another position in an array, overwriting the existing values.
-
 The copyWithin() does not add items to the array.
-
 copyWithin() overwrites the original array not create a new array.
 
-Change all arguments and see what happening
+array.copyWithin(target, start, end)
+
+Parameter	    Description
+target	         Required. The index position to copy the elements to
+start	         Optional. The index position to start copying elements from  (default is 0)
+end	Optional.    The index position to stop copying elements from (default is array.length)
  */
 
 const fruits = ["Banana", "Orange", "Apple", "Apricot", "Mango", "Dates"];
