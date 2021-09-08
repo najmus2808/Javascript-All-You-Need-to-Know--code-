98fc005435a1b5009aab55c4838916a13303eb1d
najmus2808 <najmus2808@gmail.com>
2021-09-08 12:04:28 +0600

    example of isArray method

---
+++ Js_array_others_method/isArray.js
@@ -0,0 +1,5 @@
+/** The isArray() method returns true if an object is an array, otherwise false. */
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+const result = Array.isArray(fruits); // Returns true
+console.log(result);
