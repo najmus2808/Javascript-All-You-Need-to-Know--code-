a27ee510bce187f7d12414ceeca9a36197852516
najmus2808 <najmus2808@gmail.com>
2021-09-09 11:17:49 +0600

    example of toString method

---
+++ Js_array_others_method/toString.js
@@ -0,0 +1,10 @@
+/** The toString() method returns a string with all array values separated by commas.
+
+toString() does not change the original array.
+
+**/
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+let text = fruits.toString();
+
+console.log(fruits);
