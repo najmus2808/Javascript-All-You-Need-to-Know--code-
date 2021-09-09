bd2cbca296841dbb6b862d339610e2271dac12d0
najmus2808 <najmus2808@gmail.com>
2021-09-09 10:25:14 +0600

    example of push method

---
+++ Js_array_others_method/push.js
@@ -0,0 +1,9 @@
+/** The push() method adds new items to the end of an array.
+
+push() changes the length of the array and returns the new length.*/
+
+const numbers = [3, 4, 9, 16, 25];
+
+numbers.push(99);
+
+console.log(numbers);
