a4944a6bee6576faca5ed5f0c6ca94d35cf96a2d
najmus2808 <najmus2808@gmail.com>
2021-09-09 10:28:03 +0600

    example of reverse method

---
+++ Js_array_others_method/reverse.js
@@ -0,0 +1,9 @@
+/**The reverse() method reverses the order of the elements in an array.
+
+reverse() overwrites the original array. */
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.reverse();
+
+console.log(fruits);
