1330bbee41dba823cb289c48c91fc79a182c0a36
najmus2808 <najmus2808@gmail.com>
2021-09-09 10:30:20 +0600

    example of shift method

---
+++ Js_array_others_method/shift.js
@@ -0,0 +1,9 @@
+/** The shift() method removes the first item of an array.
+
+shift() returns the element it removes.
+
+shift() changes the original array. */
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.shift();
+console.log(fruits);
