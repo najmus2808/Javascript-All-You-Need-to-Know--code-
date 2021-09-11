956521bb8caccaaf4160b79170ba92816c22bd2f
najmus2808 <najmus2808@gmail.com>
2021-09-11 09:31:02 +0600

    example of valueOf method

---
+++ Js_array_others_method/valueOf.js
@@ -0,0 +1,10 @@
+/** The valueOf() method returns the array itself.
+
+valueOf() is the default method of the array object.
+
+valueOf() does not change the original array. */
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.valueOf(); // Returns fruits
+
+console.log(fruits);
