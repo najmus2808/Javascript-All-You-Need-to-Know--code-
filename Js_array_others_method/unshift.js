897a9b5ead4cf6cce97560e1b526c62eed0bac34
najmus2808 <najmus2808@gmail.com>
2021-09-11 09:27:33 +0600

    example of unshift method

---
+++ Js_array_others_method/unshift.js
@@ -0,0 +1,10 @@
+/**The unshift() method adds new items to the beginning of an array, and returns the new length.
+
+unshift() overwrites the original array.
+array.unshift(item1, item2, ..., itemX) */
+
+
+const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.unshift("Lemon","Pineapple");
+
+console.log(fruits);
