062621fc6e7ad3a273a8591ea56ad497c2bca731
najmus2808 <najmus2808@gmail.com>
2022-01-01 23:27:19 +0600

    check palindrome or not

---
+++ Js_problem_solve/palindrome.js
@@ -0,0 +1,12 @@
+function palindrome(str) {
+  let re = /[^A-Za-z0-9]/g;
+  str = str.toLowerCase().replace(re, "");
+  let len = str.length;
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+console.log(palindrome("madam"));
